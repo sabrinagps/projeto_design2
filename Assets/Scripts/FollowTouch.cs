e745c9c1aa91b9e230b69bdb7a3d2a0da41da95a
Sabrina Goncalves <sabrinagpsilva2013@gmail.com>
2019-04-29 09:08:10 -0300

    Acrescentando Boy

--- Assets/Scripts/FollowTouch.cs
+++ Assets/Scripts/FollowTouch.cs
@@ -10,10 +10,10 @@ public class FollowTouch : MonoBehaviour
     {
         if (Input.GetButton("Fire1")){
             Vector3 aux = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector3 inicialPosition = transform.position;
+            Vector3 initialPosition = transform.position;
             Vector3 finalPosition = new Vector3(aux.x, transform.position.y, 0);
 
-            transform.position = Vector3.Lerp(inicialPosition, finalPosition, lerpRate * Time.deltaTime);
+            transform.position = Vector3.Lerp(initialPosition, finalPosition, lerpRate * Time.deltaTime);
         }
     }
 }
